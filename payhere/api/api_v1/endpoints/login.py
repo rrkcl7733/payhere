f08cd4330a24581539c07772e96415c9b86babf5
민경욱 <rrkcl7733@naver.com>
2023-01-04 01:53:57 +0900

    user 형식 usercreate -> oauth form 으로 변경

--- payhere/api/api_v1/endpoints/login.py
+++ payhere/api/api_v1/endpoints/login.py
@@ -1,13 +1,17 @@
 from datetime import timedelta
+
+import pydantic
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 
 from payhere.core.config import settings
 from payhere.core.security import create_access_token
 from payhere.db.session import Base, engine, get_db
-from payhere.schemas.user import UserCreate
+from payhere.schemas.user import UserBase
 from payhere.models.user import User
 from payhere.core.security import get_password_hash, verify_password
+from payhere.api.depends import get_current_user
 
 Base.metadata.create_all(bind=engine)
 
@@ -15,12 +19,12 @@ router = APIRouter()
 
 
 @router.post("/register")
-def post(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.email == user.email).first()
+def post(user: UserBase, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.username).first()
     if db_user:
         raise HTTPException(status=400, detail="등록된 이메일입니다")
     db_obj = User(
-        email=user.email,
+        email=user.username,
         password=get_password_hash(user.password)
     )
     db.add(db_obj)
@@ -30,8 +34,8 @@ def post(user: UserCreate, db: Session = Depends(get_db)):
 
 
 @router.post("/login")
-async def login_for_access_token(user: UserCreate, db: Session = Depends(get_db)):
-    db_user = db.query(User).filter(User.email == user.email).first()
+async def login_for_access_token(user: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.username).first()
     if not db_user or not verify_password(user.password, db_user.password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
@@ -42,3 +46,7 @@ async def login_for_access_token(user: UserCreate, db: Session = Depends(get_db)
     access_token = create_access_token(subject=db_user.email, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
 
+
+@router.get("/test")
+async def test(current_user: User = Depends(get_current_user)):
+    return "good boy"
