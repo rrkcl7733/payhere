fdb1163f6497c02e1fbd22b4b89094dc932b8ddb
민경욱 <rrkcl7733@naver.com>
2023-01-03 16:29:54 +0900

    user model

---
+++ app/models/user.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Boolean, Column, String
+from sqlalchemy.dialects.mysql import INTEGER
+from app.db.session import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(INTEGER, primary_key=True, index=True)
+    email = Column(String(255), unique=True, index=True)
+    password = Column(String(255))
+
+    def __repr__(self):
+        return "유저 정보는 '%s', '%s'" % (
+            self.id, self.email
+        )
