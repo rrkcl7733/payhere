f55eab9f6cdb88a81f6eb282829fdc5024febabd
민경욱 <rrkcl7733@naver.com>
2023-01-03 22:59:08 +0900

    api router 추가

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
 
+from payhere.api.api import api_router
+from payhere.core.config import settings
+from payhere.db.session import SessionLocal
+
 
 app = FastAPI()
+app.include_router(api_router, prefix=settings.API_V1_STR)
