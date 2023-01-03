2fa03702a98bb07ab6af1ff889092582850ebf53
민경욱 <rrkcl7733@naver.com>
2023-01-03 16:07:52 +0900

    config

---
+++ app/core/config.py
@@ -0,0 +1,26 @@
+import os
+
+from dotenv import load_dotenv
+from pydantic import BaseSettings
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = os.getenv('SECRET_KEY')
+    ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7
+
+    HOST: str = os.getenv('HOST')
+    PORT: str = os.getenv('PORT')
+    USERNAME: str = os.getenv('USERNAME')
+    PASSWORD: str = os.getenv('PASSWORD')
+    DATABASE: str = os.getenv('DATABASE')
+    SQLALCHEMY_DATABASE_URL = f'mysql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
