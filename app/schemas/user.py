ab5b7045064d5144a67df52e0d9db8f4cfd0e8b9
민경욱 <rrkcl7733@naver.com>
2023-01-03 17:07:23 +0900

    user schema

---
+++ app/schemas/user.py
@@ -0,0 +1,27 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    email: EmailStr
+    is_active: Optional[bool] = True
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserUpdate(UserBase):
+    password: Optional[str] = None
+
+
+class UserInDBBase(UserBase):
+    id: int
+    password: Optional[str] = None
+
+    class Config:
+        orm_mode = True
+
+
+class UserRead(UserInDBBase):
+    pass
