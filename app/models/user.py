31eb7be8b4324cbd6db76dd060c400c1a2c8d2e2
민경욱 <rrkcl7733@naver.com>
2023-01-03 16:58:03 +0900

    user 정보 is_active 활성화 컬럼 추가

--- app/models/user.py
+++ app/models/user.py
@@ -9,6 +9,7 @@ class User(Base):
     id = Column(INTEGER, primary_key=True, index=True)
     email = Column(String(255), unique=True, index=True)
     password = Column(String(255))
+    is_active = Column(Boolean, default=True)
 
     def __repr__(self):
         return "유저 정보는 '%s', '%s'" % (
