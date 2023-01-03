f624121c0a47d294c486e929484e23adac1b2646
민경욱 <rrkcl7733@naver.com>
2023-01-03 22:59:23 +0900

    회원가입 쿼리

--- payhere/api/api_v1/endpoints/login.py
+++ payhere/api/api_v1/endpoints/login.py
@@ -6,6 +6,8 @@ from payhere.core.config import settings
 from payhere.core.security import create_access_token
 from payhere.db.session import Base, engine, get_db
 from payhere.schemas.user import UserCreate
+from payhere.models.user import User
+from payhere.core.security import get_password_hash
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,3 +16,14 @@ router = APIRouter()
 
 @router.post("/register")
 def post(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+    if db_user:
+        raise HTTPException(status=400, detail="등록된 이메일입니다")
+    db_obj = User(
+        email=user.email,
+        password=get_password_hash(user.password)
+    )
+    db.add(db_obj)
+    db.commit()
+    db.refresh(db_obj)
+    del db_obj.password
