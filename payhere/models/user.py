0cad5edef110b9a9c5abafc6e700bec58dcac1be
민경욱 <rrkcl7733@naver.com>
2023-01-04 22:57:00 +0900

    user에서 가계부 외래키 관계 명시(relationship)

--- payhere/models/user.py
+++ payhere/models/user.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, String
 from sqlalchemy.dialects.mysql import INTEGER
+from sqlalchemy.orm import relationship
+
 from payhere.db.session import Base
 
 
@@ -9,6 +11,8 @@ class User(Base):
     id = Column(INTEGER, primary_key=True, index=True)
     email = Column(String(255), unique=True)
     password = Column(String(255))
+
+    accounts = relationship("Account", back_populates="users")
 
     def __repr__(self):
         return "유저 정보는 '%s', '%s'" % (
