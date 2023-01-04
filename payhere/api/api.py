c2dc0f2be2557b6b146fb8e7c546f8de818f8aa7
민경욱 <rrkcl7733@naver.com>
2023-01-04 22:55:19 +0900

    가계부 router 설정

--- payhere/api/api.py
+++ payhere/api/api.py
@@ -1,6 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
-from payhere.api.api_v1.endpoints import login
+from payhere.api.api_v1.endpoints import login, account
 
 api_router = APIRouter()
-api_router.include_router(login.router, tags=["login"])
+api_router.include_router(
+    login.router,
+    tags=["login"])
+api_router.include_router(
+    account.router,
+    prefix="/accounts",
+    tags=["account"],
+    # dependencies=[Depends(get_current_user)]
+)
