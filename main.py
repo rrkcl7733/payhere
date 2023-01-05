1d9dc4bb2afa2448b48a3f42356f3ccffa27d186
민경욱 <rrkcl7733@naver.com>
2023-01-05 14:00:48 +0900

    cors 추가

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from payhere.api.api import api_router
 from payhere.core.config import settings
@@ -6,3 +7,10 @@ from payhere.core.config import settings
 
 app = FastAPI()
 app.include_router(api_router, prefix=settings.API_V1_STR)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[f"http://127.0.0.1:8000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
